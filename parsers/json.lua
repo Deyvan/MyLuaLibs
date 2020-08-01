0f02091925734903f7c8d612d42d687e496606c2
Deyvan <deyvanthebest@protonmail.com>
2020-08-01 16:35:09 +0300

    version: (unsigned int)-1

--- parsers/json.lua
+++ parsers/json.lua
@@ -1,1 +1,13 @@
---haha stupid github
+
+local function minifier(stringJson) end
+local function unminifier(stringJson) end
+
+local function toTable(stringJson) end
+local function toJson(tbl) end
+
+return {
+    minifier = minifier,
+    unminifier = unminifier,
+    toTable = toTable,
+    toJson = toJson
+}
