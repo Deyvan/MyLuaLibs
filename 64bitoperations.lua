9cc98b62b6965c571015625f6e3ce9d5d34c8e2a
Deyvan <deyvanthebest@protonmail.com>
2020-10-14 22:58:33 +0300

    optimization sum operation

--- 64bitoperations.lua
+++ 64bitoperations.lua
@@ -3,43 +3,36 @@
 
 require"bit"
 
--- L and R - 32bit int
--- (L .. R) - 64 bit int
--- tobit(num) = num % 0xFFFFFFFF
+-- H and L - 32bit int
+-- (H .. L) - 64 bit int
 
 local bor = bit.bor
 local tobit = bit.tobit
 local lshift = bit.lshift
 local rshift = bit.rshift
+local floor = math.floor
 
-local function overflowbit32(L, R)
-	if L < 0 then L = 0x100000000 + L end
-	if R < 0 then R = 0x100000000 + R end
-	if (L+R) > 0xFFFFFFFF then return 1 end
-	return 0
-end
-
---rotate left (L .. R) <<< n
+--rotate left (H .. L) <<< n
 local result_L = bor(rshift(R, 32-n), lshift(L, n))
 local result_R = bor(lshift(R, n), rshift(L, 32-n))
 
---rotate right (L .. R) >>> n
+--rotate right (H .. L) >>> n
 local result_L = bor(lshift(R, 32-n), rshift(L, n))
 local result_R = bor(rshift(R, n), lshift(L, 32-n))
 
 
 
---shift left (L .. R) << n
+--shift left (H .. L) << n
 local result_L = bor(rshift(R, 32-n), lshift(L, n))
 local result_R = lshift(R, n)
 
---shift right (L .. R) >> n
+--shift right (H .. L) >> n
 local result_L = rshift(L, n)
 local result_R = bor(rshift(R, n), lshift(L, 32-n))
 
 
 
---sum (L0 .. R0) + (L1 .. R1)
-local result_L = tobit(L0 + L1 + overflowbit32(R0, R1))
+--sum (H0 .. L0) + (H1 .. L1)
+local result_L = tobit(L0 + L1 + floor((R0 % 0x100000000 + R1 % 0x100000000) / 0x100000000))
 local result_R = tobit(R0 + R1)
 
