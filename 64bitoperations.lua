e5d2cc468a017d3309010e04cf2114f17b1ecb8a
Deyvan <deyvanthebest@protonmail.com>
2020-10-14 23:01:23 +0300

    Update 64bitoperations.lua

--- 64bitoperations.lua
+++ 64bitoperations.lua
@@ -13,26 +13,26 @@ local rshift = bit.rshift
 local floor = math.floor
 
 --rotate left (H .. L) <<< n
-local result_L = bor(rshift(R, 32-n), lshift(L, n))
-local result_R = bor(lshift(R, n), rshift(L, 32-n))
+local H = bor(rshift(R, 32-n), lshift(L, n))
+local L = bor(lshift(R, n), rshift(L, 32-n))
 
 --rotate right (H .. L) >>> n
-local result_L = bor(lshift(R, 32-n), rshift(L, n))
-local result_R = bor(rshift(R, n), lshift(L, 32-n))
+local H = bor(lshift(R, 32-n), rshift(L, n))
+local L = bor(rshift(R, n), lshift(L, 32-n))
 
 
 
 --shift left (H .. L) << n
-local result_L = bor(rshift(R, 32-n), lshift(L, n))
-local result_R = lshift(R, n)
+local H = bor(rshift(R, 32-n), lshift(L, n))
+local L = lshift(R, n)
 
 --shift right (H .. L) >> n
-local result_L = rshift(L, n)
-local result_R = bor(rshift(R, n), lshift(L, 32-n))
+local H = rshift(L, n)
+local L = bor(rshift(R, n), lshift(L, 32-n))
 
 
 
 --sum (H0 .. L0) + (H1 .. L1)
-local result_L = tobit(L0 + L1 + floor((R0 % 0x100000000 + R1 % 0x100000000) / 0x100000000))
-local result_R = tobit(R0 + R1)
+local H = tobit(L0 + L1 + floor((R0 % 0x100000000 + R1 % 0x100000000) / 0x100000000))
+local L = tobit(R0 + R1)
 
